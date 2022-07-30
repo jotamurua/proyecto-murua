8d11eb85b27ce597d5a142a94e70cc97cde05297
jotamurua <102777812+jotamurua@users.noreply.github.com>
2022-07-30 11:38:42 -0300

    work and ed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ProfileComponent
+    ProfileComponent,
+    ExperienceComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule
