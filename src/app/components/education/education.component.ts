f8a3f5269aab6f158c884b743acaae65d9c70897
jotamurua <102777812+jotamurua@users.noreply.github.com>
2022-08-02 18:51:49 -0300

    education

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-education',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EducationComponent implements OnInit {
 
-  constructor() { }
+  educArticle:any;
+
+
+  constructor(private datosPortfolio:PortfolioService) {
+   }
 
   ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe(data => {
+      this.educArticle=data.education;
+    });
   }
 
 }
