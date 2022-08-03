84f1edb7849f8e1465382dc919a2e89362513d30
jotamurua <102777812+jotamurua@users.noreply.github.com>
2022-08-02 21:49:48 -0300

    json finished

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
+
 
 @Component({
   selector: 'app-projects',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  projectsArticle:any;
+
+
+  constructor(private datosPortfolio:PortfolioService) {
+   }
 
   ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe(data => {
+      this.projectsArticle=data.projects;
+    });
   }
 
 }
